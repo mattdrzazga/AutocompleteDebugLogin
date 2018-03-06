3a0effd6fc5c63bc6e3cea453c44b170e5fb8913
Mateusz Drzazga <matt.drzazga@gmail.com>
2018-03-06 22:05:13 +0100

    Initial commit.

---
+++ app/src/main/java/com/example/matt/autocompletedebuglogin/LoginActivity.kt
@@ -0,0 +1,80 @@
+package com.example.matt.autocompletedebuglogin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.view.View
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private fun attemptLogin() {
+        // Reset errors.
+        email.error = null
+        password.error = null
+
+        // Store values at the time of the login attempt.
+        val emailStr = email.text.toString()
+        val passwordStr = password.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(passwordStr) && !isPasswordValid(passwordStr)) {
+            password.error = getString(R.string.error_invalid_password)
+            focusView = password
+            cancel = true
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(emailStr)) {
+            email.error = getString(R.string.error_field_required)
+            focusView = email
+            cancel = true
+        } else if (!isEmailValid(emailStr)) {
+            email.error = getString(R.string.error_invalid_email)
+            focusView = email
+            cancel = true
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView?.requestFocus()
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 4
+    }
+
+    private fun addEmailsToAutoComplete(emailAddressCollection: List<String>) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        val adapter = ArrayAdapter(this@LoginActivity,
+                android.R.layout.simple_dropdown_item_1line, emailAddressCollection)
+
+        email.setAdapter(adapter)
+    }
+}
